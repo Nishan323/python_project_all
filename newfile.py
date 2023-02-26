abdde3675a799809c80367deb2d1c558711fb490
Nishan <mr.raiali323@gmail.co>
2023-02-26 16:15:24 +0545

    Area of circle by Nishan a

--- newfile.py
+++ newfile.py
@@ -2,3 +2,9 @@ def findArea(r):
     PI = 3.14
     return PI * (r*r);
 print("Area of circle = %.6f" % findArea(6));
+
+
+import math
+r = float(input("Enter the radius of a circle:"))
+area = math.pi * r * r
+print("Area of a circle = %.2f" %area)
